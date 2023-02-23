e72db6355133b92bf94fcb1d0f11fd929f570f7f
Toomas <toomas.kirsing@gmail.com>
2023-02-23 16:46:29 +0200

    Update kirsing_kodu_2.java

--- kirsing_kodu_2.java
+++ kirsing_kodu_2.java
@@ -13,8 +13,11 @@ public class kirsing_kodu_2 {
         System.out.println("Ülesanne 1: ");
         System.out.println(ül1());
         System.out.println("Ülesanne 2:");
-         //ül2(Fail kust loetakse andmed, väljundfail õiges formaadis andmetele, väljundfail vales formaadis andmetele)
-        ül2("kodu2_andmed.txt","kodu2_õiged.txt","kodu2_valed.txt");
+        //Fail kust loetakse andmed, väljundfail õiges formaadis andmetele, väljundfail vales formaadis andmetele
+        String sisendandmed = "kodu2_andmed.txt", väljund1 = "kodu2_õiged.txt", väljund2="kodu2_valed.txt";
+        //Fail kust otsitakse rida ning otsitav sõna
+        String sisendandmed2="kodu2_andmed2.txt", otsitav = "Jaanus";
+        ül2(sisendandmed, väljund1, väljund2, sisendandmed2, otsitav);
     }
     static char leiaKõigeSagedasemNumber(String text){
         Map<Character, Integer> hm = new HashMap<Character, Integer>(); //numbrite loendamiseks hashmap
@@ -69,9 +72,10 @@ public class kirsing_kodu_2 {
         if(symbol == 'e') return "Tekstis pole numbreid!"; //Kui numbreid ei leidunud
         return eemaldaKõigeSagedasemNumber(text, symbol); //eemaldatakse number ja tagastatakse muudetud tekst
     }
+
     static void convert(String sisendfail, String väljund1, String väljund2){
         File sisend = new File(sisendfail), valjund1 = new File(väljund1), valjund2 = new File(väljund2);
-        FileWriter filewriter = null;
+        FileWriter writer = null;
         String[][] vastused1={};
         String[] vastused2={};
         Boolean õige = true;
@@ -79,18 +83,12 @@ public class kirsing_kodu_2 {
         try {
             if(!valjund1.createNewFile()){
                 valjund1.delete();
-                boolean fail1 = valjund1.createNewFile();
+                boolean fail = valjund1.createNewFile();
             }
             if(!valjund2.createNewFile()){
                 valjund2.delete();
-                boolean fail2 = valjund2.createNewFile();
+                boolean fail = valjund2.createNewFile();
             }
-        } catch (IOException e) {
-            System.out.println("Failide muutmisel esines viga!");
-            e.printStackTrace();
-            System.exit(0);
-        }
-        try{
             Scanner reader = new Scanner(sisend);
             while(reader.hasNextLine()){
                 õige = true;
@@ -101,21 +99,25 @@ public class kirsing_kodu_2 {
                 }
                 if(count == 3){ //Kui andmed on õiges formaadis
                     String[] sisendid = andmed.split("\\|");
-                    System.out.println(Arrays.toString(sisendid));
-                    try{ //kui isikukoodi pole võimalik teha numbriformaati ehk pole isikukood, ei hakka isikukoodi süvakuti kontrolli tegema näiteandmete pärast
-                        int tmp = Integer.parseInt(sisendid[0]);
-                        isikukood = sisendid[0];
+                    isikukood  = sisendid[0];
+                    if(isikukood.length() == 11) {//Isikukoodi pikkuse kontroll
+                        if(isikukood.charAt(0) >= '1' && isikukood.charAt(0) <= '6'){ //isikukoodis aasta kontroll
+                            //isikukoodis kuu kontroll, ei hakka siia kuude päevade täpset kontrolli panema
+                            if((isikukood.charAt(1) == '0' && isikukood.charAt(2) >= '1' &&  isikukood.charAt(2) <= '9') || (isikukood.charAt(1) == '1' && isikukood.charAt(2) >= '1' &&  isikukood.charAt(2) <= '2')){
+                                if(!(isikukood.charAt(5) == '0' && isikukood.charAt(6) >= '1' && isikukood.charAt(6) <= '9') && !(isikukood.charAt(5) >= '1' && isikukood.charAt(5) <= '2' && isikukood.charAt(6) >= '0' && isikukood.charAt(6) <= '9') && !(isikukood.charAt(5) == '3' && isikukood.charAt(6) >= '0' && isikukood.charAt(6) <= '1')){
+                                    õige=false;
+                                }
+                            }
+                            else õige = false;
+                        }
+                        else õige = false;
                     }
-                    catch (NumberFormatException ex){
-                        õige = false;
-                    }
-                    try{ //kui palka pole võimalik teha numbriformaati ehk pole isikukood, ei hakka isikukoodi süvakuti kontrolli tegema näiteandmete pärast
+                    else õige = false;
+                    try{ //kui palka pole võimalik teha numbriformaati
                         int tmp = Integer.parseInt(sisendid[3]);
                         palk = sisendid[3];
                     }
-                    catch (NumberFormatException ex){
-                        õige = false;
-                    }
+                    catch (NumberFormatException ex){õige = false;}
                     perenimi = sisendid[1];
                     eesnimi = sisendid[2];
                     if(õige){ //kui kontroll läbitud edukalt kirjutatakse faili
@@ -123,29 +125,72 @@ public class kirsing_kodu_2 {
                         int n = vastused1.length;
                         vastused1 = Arrays.copyOf(vastused1, n+1);
                         vastused1[n] = temp;
-
                     }
                     else{ //kontrolli ei läbitud kirjutatakse valede faili
                         int n = vastused2.length;
                         vastused2 = Arrays.copyOf(vastused2, n+1);
-                        vastused1[n] = andmed;
+                        vastused2[n] = andmed;
                     }
                 }
                 else{ //kontrolli ei läbitud kirjutatakse valede faili
-                    filewriter = new FileWriter(valjund2);
-                    filewriter.write(andmed);
-                    filewriter.close();
+                    int n = vastused2.length;
+                    vastused2 = Arrays.copyOf(vastused2, n+1);
+                    vastused2[n] = andmed;
                 }
             }
             reader.close();
-        }
-        catch(IOException e){ //kui sisendfaili ei leidu tagastatakse veateade ja väljutakse
-            System.out.println("Sisendfaili ei leitud!");
+            writer = new FileWriter(valjund1);
+            for(int i = 0; i < vastused1.length; i++){
+                for(int j = 0; j < vastused1[i].length; j++){
+                    writer.write(vastused1[i][j]+"\n");
+                }
+                writer.write("\n");
+            }
+            writer.close();
+            writer = new FileWriter(valjund2);
+            for(int i = 0; i < vastused2.length; i++){
+                writer.write(vastused2[i]+"\n");
+            }
+            writer.close();
+            System.out.println("Failid muudetud edukalt!");
+        } catch(IOException e){ //kui sisendfaili ei leidu tagastatakse veateade ja väljutakse
+            System.out.println("Failide muutmisel esine viga!");
             e.printStackTrace();
-            System.exit(0);
         }
     }
-    static void ül2(String sisend, String väljund1, String väljund2){
+    static void eemaldaRida(String sisendfail, String otsitav){
+        File sisend = new File(sisendfail);
+        String[] tulemus = {};
+        try {
+            Scanner reader = new Scanner(sisend);
+            while(reader.hasNextLine()){
+                String rida = reader.nextLine();
+                if(!rida.contains(otsitav)){ //kui sisse loetud rida ei sisalda otsitavat sõne salvestatakse see muutujasse ja kirjutatakse hiljem faili
+                    int n = tulemus.length;
+                    tulemus = Arrays.copyOf(tulemus, n+1);
+                    tulemus[n] = rida;
+                }
+            }
+            if(!sisend.createNewFile()){
+                sisend.delete();
+                boolean fail = sisend.createNewFile();
+            }
+            reader.close();
+            FileWriter writer = new FileWriter(sisendfail);
+            for(int i = 0; i < tulemus.length; i++){
+                writer.write(tulemus[i]+"\n");
+            }
+            writer.close();
+            System.out.println("Rida eemaldatud edukalt!");
+        } catch (IOException e) {
+            System.out.println("Rea eemaldamisel esines viga!");
+            e.printStackTrace();
+        }
+    }
+    static void ül2(String sisend, String väljund1, String väljund2, String sisend2, String otsitav){
+        System.out.println("Andmete sorteerimine...");
         convert(sisend, väljund1, väljund2);
+        System.out.println("Eemaldan soovitud rea...");
+        eemaldaRida(sisend2, otsitav);
     }
 }
